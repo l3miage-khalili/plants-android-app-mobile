dcb5cfd4439b4b76b39454c6ac59becef70675e5
l3miage-khalili <khalilbrahimgoukouni@gmail.com>
2024-04-13 22:38:01 +0200

    rajoute le recyclerView verticale des plantes dans le fragment home

--- app/src/main/java/com/example/myapplication/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/HomeFragment.kt
@@ -18,9 +18,14 @@ class HomeFragment : Fragment() {
         // représentation que le système Android peut comprendre et afficher à l'utilisateur à l'écran
         val view =  inflater.inflate(R.layout.fragment_home, container, false)
 
-        // recupération du recyclerView
+        // recupération du recyclerView horizontal
         val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
-        horizontalRecyclerView.adapter = PlantAdapter()
+        horizontalRecyclerView.adapter = PlantAdapter(R.layout.item_horizontal_plant)
+
+
+        // recupération du recyclerView vertical
+        val verticalRecyclerView = view.findViewById<RecyclerView>(R.id.vertical_recycler_view)
+        verticalRecyclerView.adapter = PlantAdapter(R.layout.item_vertical_plant)
 
         return view
     }
