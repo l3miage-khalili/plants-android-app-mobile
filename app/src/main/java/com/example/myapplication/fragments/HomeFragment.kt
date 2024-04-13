2dc6766798908a6215e998b3495574d61f52d46a
l3miage-khalili <khalilbrahimgoukouni@gmail.com>
2024-04-13 21:19:52 +0200

    adapte les images et ajoute le champ Dernières plantes au fragment home

--- app/src/main/java/com/example/myapplication/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.adapter.PlantAdapter
 
 class HomeFragment : Fragment() {
 
@@ -14,6 +16,12 @@ class HomeFragment : Fragment() {
         // utilise un LayoutInflater (ici inflater) qui permet de créer la vue
         // sa fonction inflate permet de transformer un fichier xml (ici fragment_home.xml) en une
         // représentation que le système Android peut comprendre et afficher à l'utilisateur à l'écran
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view =  inflater.inflate(R.layout.fragment_home, container, false)
+
+        // recupération du recyclerView
+        val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
+        horizontalRecyclerView.adapter = PlantAdapter()
+
+        return view
     }
 }
