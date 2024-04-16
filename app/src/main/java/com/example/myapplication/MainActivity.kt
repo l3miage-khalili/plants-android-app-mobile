221fb7a6f085f81e1535c94703292c443e7c60ec
l3miage-khalili <khalilbrahimgoukouni@gmail.com>
2024-04-16 02:24:54 +0200

    réalise la partie finale de l'app - navigation entre différents fragments

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.myapplication.fragments.AddPlantFragment
 import com.example.myapplication.fragments.CollectionFragment
 import com.example.myapplication.fragments.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,8 +29,38 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        loadFragment(HomeFragment(this), R.string.home_page_title)
+
+        // importation du botton navigation view
+        val navigationView = findViewById<BottomNavigationView>(R.id.navigation_view)
+        navigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home_page -> {
+                    loadFragment(HomeFragment(this), R.string.home_page_title)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.collection_page -> {
+                    loadFragment(CollectionFragment(this), R.string.collection_page_title)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.add_plant_page -> {
+                    loadFragment(AddPlantFragment(this), R.string.add_plant_page_title)
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                else -> {false}
+            }
+        }
+        loadFragment(HomeFragment(this), R.string.home_page_title)
+    }
+
+    private fun loadFragment(fragment: Fragment, string: Int) {
+
         // chargement du repository
         val repo = PlantRepository()
+
+        // actualisation du titre de la page
+        findViewById<TextView>(R.id.page_title).text = resources.getString(string)
 
         // charge et met à jour la liste des plantes, son contenu est excécuté comme callback après le chargement de la liste
         repo.updateData{
@@ -37,7 +70,7 @@ class MainActivity : AppCompatActivity() {
             * beginTransaction() commence une série des opérations pour pouvoir manipuler les fragments
             * */
             val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, HomeFragment(this))
+            transaction.replace(R.id.fragment_container, fragment)
             transaction.addToBackStack(null)
             transaction.commit()
         }
